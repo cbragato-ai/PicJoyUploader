de140688ddcc8fb7e6b2d1c6a55f21239e7be378
Clodoaldo <cbragato@gmail.com>
2026-02-10 10:23:34 -0300

    Set vite port

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vuetify from "vite-plugin-vuetify";
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
   server: {
-    port: 3000,
+    port: 5173,
     open: true,
     allowedHosts: ["localhost", "uploader.picjoy.com.br"],
   },
